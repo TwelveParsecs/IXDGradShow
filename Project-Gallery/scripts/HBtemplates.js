c10da10bc6a3d663229eee137910a72b4a4e9e2d
maozillah <kayexmao@gmail.com>
2017-04-07 01:15:07 -0400

    updated json url

--- Project-Gallery/scripts/HBtemplates.js
+++ Project-Gallery/scripts/HBtemplates.js
@@ -3,8 +3,9 @@ Handlebars.registerHelper("counter", function (index){
 });
 
 // var jsonData = "https://twelveparsecs.github.io/IXDGradShow/Project-Gallery/json/ixd_projects.json";
+var jsonData = "https://twelveparsecs.github.io/IXDGradShow/Project-Gallery/json/ixd_projects_2.json";
 
-var jsonData = "json/ixd_projects_2.json";
+// var jsonData = "json/ixd_projects_2.json";
 
 // asynchronous call
 $.getJSON(jsonData, function(json) {
