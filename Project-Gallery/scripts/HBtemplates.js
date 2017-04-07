622adc15cdaa35c445e911e3334a0dee341c22e6
maozillah <kayexmao@gmail.com>
2017-04-07 01:14:23 -0400

    updated some data

--- Project-Gallery/scripts/HBtemplates.js
+++ Project-Gallery/scripts/HBtemplates.js
@@ -2,9 +2,9 @@ Handlebars.registerHelper("counter", function (index){
     return index + 1;
 });
 
-var jsonData = "https://twelveparsecs.github.io/IXDGradShow/Project-Gallery/json/ixd_projects.json";
+// var jsonData = "https://twelveparsecs.github.io/IXDGradShow/Project-Gallery/json/ixd_projects.json";
 
-// var jsonData = "json/ixd_projects.json";
+var jsonData = "json/ixd_projects_2.json";
 
 // asynchronous call
 $.getJSON(jsonData, function(json) {
@@ -19,4 +19,16 @@ $.getJSON(jsonData, function(json) {
     var modalScript = Handlebars.compile(modal);
     var html2 = modalScript(json);
     $("#modal").append(html2);
+});
+
+Handlebars.registerHelper("debug", function(optionalValue) {
+  console.log("Current Context");
+  console.log("====================");
+  console.log(this);
+ 
+  if (optionalValue) {
+    console.log("Value");
+    console.log("====================");
+    console.log(optionalValue);
+  }
 });
